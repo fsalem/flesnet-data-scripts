026c645121d12e6a31d2dc9018c1c282f6017552
fsalem <farouqsalem@gmail.com>
2018-01-16 12:57:06 +0100

    more scripts for the speedup

---
+++ DDS/compute.interval_speedup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source "common.sh"
+
+FILE_NAME="${array[0]}/${array[1]}.compute.min_max_interval_info.out"
+
+LABEL="set xlabel 'Interval index'; set ylabel 'Speedup factor'; set yrange [0:1]; set title"
+CMD_F1="$LABEL 'Intervals Speedup factors of Compute node#${array[1]} in $JOB_NAME [$INPUT_COUNT INs, $COMPUTE_COUNT CNs, $INPUT_BW GB/s] [$DATE]'; plot "
+
+CMD_F1="$CMD_F1'$FILE_NAME' using 1:6 with linespoints title 'Factor' "
+#echo "CMD_F1=$CMD_F1"
+gnuplot -e "$CMD_F1; pause -1"
---
+++ DDS/compute.proposed_median_enhanced_duration.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source "common.sh"
+
+FILE_NAME="${array[0]}/${array[1]}.compute.proposed_median_enhanced_duration.out"
+
+LABEL="set xlabel 'Interval index'; set ylabel 'Duration in ms'; set xtics 0,1; set title"
+CMD_F1="$LABEL 'Proposed vs Actual durations for intervals of Compute node#${array[1]} in $JOB_NAME [$INPUT_COUNT INs, $COMPUTE_COUNT CNs, $INPUT_BW GB/s] [$DATE]'; plot "
+
+CMD_F1="$CMD_F1'$FILE_NAME' using 1:(\$2/1000) with points title 'Proposed duration', "
+
+CMD_F1="$CMD_F1'$FILE_NAME' using 1:(\$3/1000) with points title 'Enhanced duration' "
+
+#echo "CMD_F1=$CMD_F1"
+gnuplot -e "$CMD_F1; pause -1"
